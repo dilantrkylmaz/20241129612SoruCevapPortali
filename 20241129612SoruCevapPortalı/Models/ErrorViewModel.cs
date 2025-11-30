034a2b47dda0a2f85e138d9107f19d6328833d8e
Dilan Türkyılmaz <dilantr063606@gmail.com>
2025-11-30 21:55:06 +0300

    Add project files.

---
+++ 20241129612SoruCevapPortalı/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20241129612SoruCevapPortalı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
