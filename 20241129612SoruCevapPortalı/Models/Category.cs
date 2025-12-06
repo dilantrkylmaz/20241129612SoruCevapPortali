8e335be7d8cb463d22a8cf8b8fad39be634e9f35
Dilan Türkyılmaz <dilantr063606@gmail.com>
2025-12-06 16:36:53 +0300

    kategori sayfası düzenlendi.

--- 20241129612SoruCevapPortalı/Models/Category.cs
+++ 20241129612SoruCevapPortalı/Models/Category.cs
@@ -11,9 +11,8 @@ namespace _20241129612SoruCevapPortalı.Models
         public string Name { get; set; }
 
         [Display(Name = "Açıklama")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        // Bir kategoride birden fazla soru olabilir
-        public ICollection<Question> Questions { get; set; }
+        public virtual ICollection<Question>? Questions { get; set; }
     }
 }
